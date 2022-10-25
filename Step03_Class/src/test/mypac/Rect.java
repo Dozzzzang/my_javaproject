185f5d367065ff99d7a2f63a0a0ececc6c84c44d
dohee <dohe00@naver.com>
2022-10-25 12:24:07 +0900

    Step04_Method 추가함

--- Step03_Class/src/test/mypac/Rect.java
+++ Step03_Class/src/test/mypac/Rect.java
@@ -1,9 +1,10 @@
 package test.mypac;
 
 public class Rect {
+	//필드
 	public int width;
 	public int height;	
-	
+	//메소드
 	public void showArea() {
 		int result = this.width*this.height; 
 		System.out.println("넓이는"+result);
