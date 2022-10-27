ca00da5dbd7274a10846a461f8a4d914239a5f13
dohee <dohe00@naver.com>
2022-10-27 11:03:45 +0900

    Step07에 Quiz 추가함

--- Step07_Array/src/test/main/Quiz.java
+++ Step07_Array/src/test/main/Quiz.java
@@ -16,8 +16,11 @@ public class Quiz {
 		 * int ranNum=ran.nextInt(5);
 		 */
 		String[] items={"cherry", "apple", "banana", "melon", "7"};
+		// 랜덤한 숫자를 얻어내기 위한 객체
 		Random ran=new Random();
+		// 0~4 사이의 랜덤한 숫자를 하나 얻어내기
 		int ranNum=ran.nextInt(5);
+		// 배열의 이덱스로 활용해서 문자열 출력하기
 		System.out.println(items[ranNum]);
 		
 		/*
